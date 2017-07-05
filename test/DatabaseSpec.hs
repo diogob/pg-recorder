a408b44c705c8a3a51c7c1c21ce9124338f1ca2b
Diogo Biazus <diogob@nulogy.com>
2017-07-05 13:01:24 -0400

    WIP

---
+++ test/DatabaseSpec.hs
@@ -0,0 +1,26 @@
+module DatabaseSpec (spec) where
+
+import Protolude
+
+import           Data.Function                        (id)
+import qualified Hasql.Connection                     as H
+import qualified Hasql.Pool                           as HP
+import Test.Hspec
+
+import PgRecorder.Database
+
+spec :: Spec
+spec =
+  describe "waitForNotifications" $
+    it "does not block the connection ann trigger action upon notification" $ do
+      conOrError <- H.acquire "postgres://localhost/postgrest_test"
+      let con = either (panic . show) id conOrError :: H.Connection
+      notification <- liftIO newEmptyMVar
+
+      waitForNotifications (curry $ putMVar notification) con
+      listen con $ toPgIdentifier "test"
+
+      pool <- HP.acquire (1, 1, "postgres://localhost/postgrest_test")
+      void $ callProcedure pool (toPgIdentifier "pg_notify") (toPgIdentifier "test") "hello there"
+
+      readMVar notification `shouldReturn` ("test", "hello there")
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,16 +1,1 @@
-import Protolude
-import PgRecorder
-import PgRecorder.Config
-import Data.Pool (withResource)
-import qualified Database.PostgreSQL.LibPQ as PQ
-import Test.Hspec
-
-main :: IO ()
-main =
-  hspec $
-    describe "PgRecorder.createExecutorsPool" $
-      it "should connect and allow operations with pool" $ do
-        pool <- createExecutorsPool pgSettings
-        void $ withResource pool $ flip PQ.exec "LISTEN frontend"
-  where
-    pgSettings = AppConfig "postgres://localhost/recorder_test" "test"
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
